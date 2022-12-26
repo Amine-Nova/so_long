1aacdf4d2dfb932847b4a1a6493d7e5ee3945cb3
Amine Benmoussa <abenmous@c1r5p5.1337.ma>
2022-12-26 22:51:04 +0100

    42

--- long.c
+++ long.c
@@ -6,12 +6,46 @@
 /*   By: abenmous <abenmous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/23 19:16:25 by abenmous          #+#    #+#             */
-/*   Updated: 2022/12/26 21:25:36 by abenmous         ###   ########.fr       */
+/*   Updated: 2022/12/26 22:48:17 by abenmous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+int my_check(char **s)
+{
+    int i;
+    int j;
+    int len;
+    int l;
 
+    l = -1;
+    i = 0;
+    j = 0;
+    while (s[i++])
+        l++;
+    i = 0;
+    len = ft_strlen(s[j]);
+    while(s[i])
+    {
+        if(i == 0 || i == l)
+        {
+            while(s[i][j])
+            {
+                if(s[i][j] == '1')
+                    j++;
+                else
+                    return(printf("%d", 1));
+            }
+        }
+        i++;
+        // if(i > 0 && i < l)
+        // {
+        //     if(s[i][0] != '1' || s[i][len] != '1')
+        //         return(printf("%d", 2));
+        // }
+    }
+    return(printf("%d", 3));
+}
 int main()
 {
     char *maj;
@@ -26,5 +60,5 @@ int main()
         line = get_next_line(fd);
     }
     maji = ft_split(maj, '\n');
-    printf("%s", maji[1]);
+    my_check(maji);
 }
